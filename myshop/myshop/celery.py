b01e890cbacdd750e035426b070d32e726683fe3
jayClean <rohitmohanty5.8.2000@gmail.com>
2024-09-08 21:41:31 +0530

    initial commit

---
+++ myshop/myshop/celery.py
@@ -0,0 +1,17 @@
+# myshop/celery.py
+
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+# Set the default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+
+app = Celery('myshop')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
